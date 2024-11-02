eab6bbe71f99b7977f92ec29416232a7c55a76ba
dimasyoga42 <dimasyoga9090@gmail.com>
2024-11-02 20:05:09 +0700

    style: mengubah font live proview

--- src/components/livepriview.jsx
+++ src/components/livepriview.jsx
@@ -2,11 +2,14 @@ import remarkGfm from "remark-gfm";
 import ReactMarkdown from 'react-markdown';
 const Live = ({markdowns}) => {
     return (
+        <>
+        <h1 className="font-poppins font-bold text-2xl uppercase">live priview:</h1>
         <div className="border border-white  lg:m-auto rounded-sm  w-full h-auto z-10">
             <div className="markdown-preview p-4 prose font-poppins w-full h-full">
   <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdowns}</ReactMarkdown>
 </div>
 </div>
+</>
     );
 }
 export default Live;
