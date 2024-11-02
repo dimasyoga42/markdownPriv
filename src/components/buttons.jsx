8ae4a895be973d19fddcc4b9ad4c6ba6ae296e9b
dimasyoga42 <dimasyoga9090@gmail.com>
2024-11-02 23:20:19 +0700

    style: ubah ukuran live preview dan juga ubah icon pada button

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,10 +1,10 @@
-import {  Edit2 } from "react-feather";
+import {   Eye } from "react-feather";
 
 const Btn = ({onClick}) => {
   return (
     <>
       <button onClick={onClick} className="fixed bottom-0 left-0 z-10 mb-4 ml-[80%] flex h-10 w-10 cursor-pointer items-center justify-center justify-items-center rounded-full lg:hidden">
-        <Edit2 color="gray" />
+        <Eye color="gray" />
       </button>
     </>
   );
--- src/components/livepriview.jsx
+++ src/components/livepriview.jsx
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 const Live = ({markdowns}) => {
   return (
     <>
-      <div className="border border-white rounded-sm  w-full lg:w-full h-auto lg:min-h-screen">
+      <div className="border border-white rounded-sm  w-full min-h-screen lg:w-full lg:h-[100vh] overflow-hidden overflow-x-hidden  overflow-scroll">
         <div className="markdown-preview p-4 prose font-poppins w-full h-full">
           <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdowns}</ReactMarkdown>
         </div>
