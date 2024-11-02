1a4fcb6e7a3de2e6ca7bffcbb3368a8a0299f9f4
dimasyoga42 <dimasyoga9090@gmail.com>
2024-11-02 20:03:04 +0700

    add: IDE

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,30 +1,59 @@
 import Btn from "../components/buttons";
 import Live from "../components/livepriview";
-import { useState } from "react";
-import { useEffect } from "react";
-const Page = ({markdowns}) => {
-  const [ markdown, setmarkdown ] = useState('')
-  const handlechange = (e) => {
-     setmarkdown(e.target.value);
-     localStorage.setItem("mark", JSON.stringify(markdown));
+import { useState, useEffect } from "react";
+import Editor from '@monaco-editor/react';
+
+const Page = ({ markdowns }) => {
+  const [markdown, setMarkdown] = useState('');
+
+  const handleChange = (value) => {
+    setMarkdown(value);
+    localStorage.setItem("mark", JSON.stringify(value));
   };
-  const handlers = () => {
-    document.getElementById('texts').classList.toggle('hidden')
-  }
+
+  const handleToggle = () => {
+    document.getElementById('texts').classList.toggle('hidden');
+  };
+
   useEffect(() => {
-    const storedName = localStorage.getItem("mark");
-    if (storedName) {
-      setmarkdown(JSON.parse(storedName));
+    const storedMarkdown = localStorage.getItem("mark");
+    if (storedMarkdown) {
+      setMarkdown(JSON.parse(storedMarkdown));
     }
   }, []);
+
   return (
     <>
-    <div className="z-0">
-      <textarea type="text" id="texts" className=" w-full  h-60  outline-none " value={markdown} onChange={handlechange}/>
-      <Btn onClick={handlers}/>
-    </div>
-     <Live markdowns={markdown}/>
+      <div className="z-0">
+      <div id="texts" className="">
+        <Editor
+          height="200px"
+          defaultLanguage="markdown"
+          defaultValue="mulai lah mengetik markdown"
+          onChange={handleChange}
+          value={markdown}
+          theme="vs-dark"
+          options={{
+            scrollbar: {
+              wordWrap: "on",
+              verticalHasArrows: false,
+              horizontalHasArrows: false,
+              verticalScrollbarSize: 8,   // Ukuran scrollbar vertikal
+              horizontalScrollbarSize: 8, // Ukuran scrollbar horizontal
+              handleMouseWheel: true,
+              alwaysConsumeMouseWheel: false,
+            },
+            minimap: { enabled: false }, // Menonaktifkan minimap jika tidak diperlukan
+            scrollBeyondLastLine: false, // Menonaktifkan scroll lebih dari akhir baris
+            hideCursorInOverviewRuler: true, // Menghilangkan kursor di overview ruler
+          }}
+        />
+       </div>
+        <Btn onClick={handleToggle} />
+      </div>
+      <Live markdowns={markdown} />
     </>
   );
 };
+
 export default Page;
