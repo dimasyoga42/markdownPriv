b4046a52dfc28cbd76e1039acd18882b0dda72c1
dimasyoga42 <dimasyoga9090@gmail.com>
2024-11-04 18:29:30 +0700

    fix: bug live preview ga bisa di scroll

--- src/components/livepriview.jsx
+++ src/components/livepriview.jsx
@@ -1,12 +1,13 @@
 import remarkGfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
 import { useEffect, useRef } from "react";
+
 const Live = ({ markdowns }) => {
   const contentRef = useRef(null);
-  //shortcute pada IDE
+
   useEffect(() => {
     const handlerkeyDow = (event) => {
-      //SHORTCUT SELECT ALL
+      // SHORTCUT SELECT ALL
       if (event.ctrlKey && event.key === "a") {
         event.preventDefault();
         const range = document.createRange();
@@ -16,17 +17,19 @@ const Live = ({ markdowns }) => {
         selection.addRange(range);
       }
     };
+
     window.addEventListener("keydown", handlerkeyDow);
     return () => {
       window.removeEventListener("keydown", handlerkeyDow);
     };
   }, []);
+
   return (
     <>
-      <div className="overflow-scrol min-h-screen w-full overflow-hidden overflow-x-hidden rounded-2xl border border-white md:w-full lg:h-[100vh]">
-        <div className="markdown-preview prose mockup-window h-full w-full rounded-md font-poppins">
-          <div className="pl-3">
-            <ReactMarkdown remarkPlugins={[remarkGfm]} ref={contentRef}>
+      <div className="h-screen w-full rounded-2xl border  border-white mockup-window">
+        <div className="markdown-preview prose  h-full w-full m-auto font-poppins overscroll-auto overflow-scroll">
+          <div className="p-3" ref={contentRef}>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
               {markdowns}
             </ReactMarkdown>
           </div>
