f23e3a82436329ba25099bff277f3255e70f1625
dimasyoga42 <dimasyoga9090@gmail.com>
2024-11-04 19:05:28 +0700

    add: docs 1.2

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -1,8 +1,25 @@
-const Dcos = () => {
+
+import { useState, useEffect } from 'react';
+import remarkGfm from "remark-gfm";
+import ReactMarkdown from "react-markdown";
+function App() {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    fetch('./../../public/docs.md')
+      .then((response) => response.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
-    <div className="flex-col gap-4 space-y-4">
-      
+    <div className='h-screen w-full lg:w-1/2 rounded-2xl'>
+      <div className="markdown-preview prose  h-full w-full m-auto font-poppins overscroll-auto overflow-scroll text-ms" >
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {markdown}
+            </ReactMarkdown>
+      </div>
     </div>
   );
-};
-export default Dcos;
+}
+
+export default App;
